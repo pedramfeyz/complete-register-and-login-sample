0dedb243ad51c62d6ba3e98e6d1ceb7421b152db
pedramfeyz <pedram_feyz@yahoo.com>
2016-07-11 21:38:02 +1000

    Update connect.php

--- register&login/core/database/connect.php
+++ register&login/core/database/connect.php
@@ -1,4 +1,4 @@
 <?php
-mysql_connect('localhost','pedramlogin','pedramlogin123') or die('could not connect:' .mysql_error());
-mysql_select_db('pedramloginlr') or die('can\'t use'.libbookstore.':'.mysql_error());
+mysql_connect('server','user','pass') or die('could not connect:' .mysql_error());
+mysql_select_db('dbName') or die('can\'t use'.libbookstore.':'.mysql_error());
 ?>
